1438b651315cc9f65e5419f7996f3902edad25b4
Muhammad Ikhsan <mikhsandev@gmail.com>
2024-09-24 13:44:35 +0700

    Update initial seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,12 +17,29 @@ class UserSeeder extends Seeder
         // Create a default user
         User::factory()
             ->create([
-                'name'  => 'Harris Sontanu',
+                'name'  => 'Administrator',
                 'username'  => 'admin',
-                'email' => 'harris.sontanu@gmail.com',
+                'email' => 'admin@jdih.sintang.go.id',
                 'role'  => UserRole::ADMIN,
+                'password' => bcrypt('AdminJdihSintang@2024'),
             ]);
 
-        User::factory()->count(15)->create();
+        User::factory()
+            ->create([
+                'name'  => 'Editor',
+                'username'  => 'editor',
+                'email' => 'editor@jdih.sintang.go.id',
+                'role'  => UserRole::EDITOR,
+                'password' => bcrypt('EditorJdihSintang@2024'),
+            ]);
+
+        User::factory()
+            ->create([
+                'name'  => 'Author',
+                'username'  => 'author',
+                'email' => 'author@jdih.sintang.go.id',
+                'role'  => UserRole::AUTHOR,
+                'password' => bcrypt('AuthorJdihSintang@2024'),
+            ]);
     }
 }
