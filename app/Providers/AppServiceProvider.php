78bc2c73f58254b4210e35434d81405d75249065
Muhammad Ikhsan <mikhsandev@gmail.com>
2024-09-24 15:31:38 +0700

    Force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Enums\LegislationRelationshipType;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Illuminate\Support\Collection;
@@ -31,5 +32,9 @@ class AppServiceProvider extends ServiceProvider
                 return $value->type->value == $type;
             });
         });
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
