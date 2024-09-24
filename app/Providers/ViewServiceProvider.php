8b449c46aac70684bc86cc2d22d5222157beec3d
Muhammad Ikhsan <mikhsandev@gmail.com>
2024-09-24 13:44:13 +0700

    Update ViewServiceProvider to handle when no tables created yet

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -26,27 +26,31 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $settings = Setting::pluck('value', 'key')->toArray();
+        if (\Schema::hasTable((new Setting)->getTable())) {
+            $settings = Setting::pluck('value', 'key')->toArray();
 
-        $settings['appLogoUrl'] = isset($settings['appLogo'])
-            ? (Storage::disk('public')->exists($settings['appLogo'])
-                ? Storage::url($settings['appLogo'])
-                : '/assets/admin/images/logo_icon.svg')
-            : '/assets/admin/images/logo_icon.svg';
-
-        $settings['jdihnLogoUrl'] = isset($settings['jdihnLogo'])
-            ? (Storage::disk('public')->exists($settings['jdihnLogo'])
-                ? Storage::url($settings['jdihnLogo'])
-                : '/assets/admin/images/jdihn-logo-web.png')
-            : '/assets/admin/images/jdihn-logo-web.png';
-
-        $settings['fullAddress'] = implode(', ', [
-            $settings['address'],
-            $settings['city'],
-            $settings['district'],
-            $settings['regency'],
-            $settings['province']
-        ]);
+            $settings['appLogoUrl'] = isset($settings['appLogo'])
+                ? (Storage::disk('public')->exists($settings['appLogo'])
+                    ? Storage::url($settings['appLogo'])
+                    : '/assets/admin/images/logo_icon.svg')
+                : '/assets/admin/images/logo_icon.svg';
+    
+            $settings['jdihnLogoUrl'] = isset($settings['jdihnLogo'])
+                ? (Storage::disk('public')->exists($settings['jdihnLogo'])
+                    ? Storage::url($settings['jdihnLogo'])
+                    : '/assets/admin/images/jdihn-logo-web.png')
+                : '/assets/admin/images/jdihn-logo-web.png';
+    
+            $settings['fullAddress'] = implode(', ', [
+                $settings['address'],
+                $settings['city'],
+                $settings['district'],
+                $settings['regency'],
+                $settings['province']
+            ]);
+        } else {
+            $settings = [];
+        }
 
         View::share($settings);
 
@@ -56,13 +60,23 @@ class ViewServiceProvider extends ServiceProvider
         );
 
         View::composer('jdih.layouts.footer', function ($view) {
-            $todayVisitor = Visitor::countDaily()->get()->count();
-            $yesterdayVisitor = Visitor::countDaily(1)->get()->count();
-            $lastWeekVisitor = Visitor::countWeekly()->get()->count();
-            $lastMonthVisitor = Visitor::countMonthly()->get()->count();
-            $allVisitor = Visitor::countAll()->get()->count();
+            if (\Schema::hasTable((new Visitor)->getTable())) {
+                $todayVisitor = Visitor::countDaily()->get()->count();
+                $yesterdayVisitor = Visitor::countDaily(1)->get()->count();
+                $lastWeekVisitor = Visitor::countWeekly()->get()->count();
+                $lastMonthVisitor = Visitor::countMonthly()->get()->count();
+                $allVisitor = Visitor::countAll()->get()->count();
+            } else {
+                $todayVisitor = 0;
+                $yesterdayVisitor = 0;
+                $lastWeekVisitor = 0;
+                $lastMonthVisitor = 0;
+                $allVisitor = 0;
+            }
 
-            $welcome = Post::whereSlug('selamat-datang')->first();
+            $welcome = (\Schema::hasTable((new Post)->getTable()))
+                ? Post::whereSlug('selamat-datang')->first()
+                : null;
 
             return $view->with('todayVisitor', $todayVisitor)
                 ->with('yesterdayVisitor', $yesterdayVisitor)
